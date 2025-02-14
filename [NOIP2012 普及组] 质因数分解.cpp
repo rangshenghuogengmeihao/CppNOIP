f3d5fb6a5751a0308a35c31970ec757a2ed70214
rangshenghuogengmeihao <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-02-14 19:59:31 +0800

    update 质因数分解

---
+++ [NOIP2012 普及组] 质因数分解.cpp
@@ -0,0 +1,15 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main() {
+  int n,a=0;
+  cin >> n;
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      cout << n/i;
+      break;
+    }
+  }
+}
