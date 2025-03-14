bde7442c043cad6869d4c88cfd92e8965da393f3
rangshenghuogengmeihao <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-03-14 21:29:15 +0800

    不与最大数相同的数字之和

---
+++ B2094 不与最大数相同的数字之和.cpp
@@ -0,0 +1,27 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int max = a[0];
+    for (int i = 0; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] != max) {
+            sum += a[i];
+        }
+    }
+    cout << sum;
+    return 0;
+}
