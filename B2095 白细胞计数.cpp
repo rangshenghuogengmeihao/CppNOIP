7d16a6cefe95934f6786f31e2c428942d6fa9f21
rangshenghuogengmeihao <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-04-11 16:57:46 +0800

    B2095 白细胞计数

---
+++ B2095 白细胞计数.cpp
@@ -0,0 +1,22 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <bits/stdc++.h>
+using namespace std;
+float a[300],sum,avg,b;
+int main(){
+    int n;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> a[i];
+        sum += a[i];
+    }
+    sort(a,a+n);
+    avg = (sum - a[0] - a[n-1])/(n-2);
+    if (avg - a[1] > a[n-2] - avg){
+        b = avg - a[1];
+    }else{
+        b = a[n-2] - avg;
+    }
+    printf("%.2f %.2f",avg,b);
+}
