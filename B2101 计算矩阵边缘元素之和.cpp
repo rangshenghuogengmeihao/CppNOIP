21b386d9919d274f7d73f4b5ba46ec17fdd58797
让生活更美好 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-05-02 13:54:53 +0800

    B2104 矩阵加法

---
+++ B2101 计算矩阵边缘元素之和.cpp
@@ -0,0 +1,25 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int a[110][110];
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                s += a[i][j];
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
