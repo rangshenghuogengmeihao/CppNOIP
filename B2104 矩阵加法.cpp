9a07479339c8c9d5cbae6b0faeb9c70ec6002641
让生活更美好 <picktime@rangshenghuogengmeihaodeMac-mini.local>
2025-05-02 13:54:53 +0800

    B2104 矩阵加法

---
+++ B2104 矩阵加法.cpp
@@ -0,0 +1,29 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int a[110][110];
+int b[110][110];
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> b[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j] + b[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
