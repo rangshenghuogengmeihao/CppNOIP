9c0bc1e881c9bc5e00663c6a9102a3671f6265a5
让生活更美好 <picktime@rangshenghuogengmeihaodeMac-mini.local>
2025-05-09 21:48:30 +0800

    B3845 [GESP样题 二级] 勾股数

---
+++ B3845 [GESP样题 二级] 勾股数.cpp
@@ -0,0 +1,20 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n,a,b,c,count=0;
+    cin >> n;
+    for (int c=1;c<=n;c++){
+        for (int b=1;b<=c;b++){
+            for (int a=1;a<=b;a++){
+                if (a*a+b*b==c*c){
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
