ed6011f150cf35f2e02cc9b014d8f76851857c36
让生活更美好 <picktime@rangshenghuogengmeihaodeMac-mini.local>
2025-05-09 21:20:52 +0800

    B3848 [GESP样题 三级] 逛商场

---
+++ B3848 [GESP样题 三级] 逛商场.cpp
@@ -0,0 +1,22 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int a[110];
+int main(){
+    int n,x,sum=0;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    cin >> x;
+    for (int i=1;i<=n;i++){
+        if (a[i]<=x){
+            x-=a[i];
+            sum+=1;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
