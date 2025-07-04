e5abfa00720290194f281af8f41d5385f0ce0e95
拾光 <picktime@Mac-mini.local>
2025-07-04 14:58:10 +0800

    B3866 [GESP202309 二级] 数字黑洞

---
+++ B3866 [GESP202309 二级] 数字黑洞.cpp
@@ -0,0 +1,20 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int a[3];
+int main() {
+    int n,res=0;
+    cin >> n;
+    while (n != 495) {
+        a[0] = n % 10;
+        a[1] = n / 10 % 10;
+        a[2] = n / 100;
+        sort(a,a+3);
+        n = a[2]*100 + a[1]*10 + a[0]-(a[0]*100+a[1]*10+a[2]);
+        res++;
+    }
+    cout << res << endl;
+    return 0;
+}
