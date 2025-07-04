438456ea2412d9979f1f64936d3ebda56f50bfbc
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-07-04 14:58:10 +0800

    B3866 [GESP202309 二级] 数字黑洞

---
+++ B3847 [GESP样题 一级] 当天的第几秒.cpp
@@ -0,0 +1,16 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int h,m,s;
+    char c;
+    cin >> h >> m >> s >> c;
+    if (c == 'A') {
+        cout << h*3600 + m*60 + s << endl;
+    } else {
+        cout << h*3600 + m*60 + s + 12*3600 << endl;
+    }
+    return 0;
+}
