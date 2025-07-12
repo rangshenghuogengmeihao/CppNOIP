c7974d369f41425797782be03730c454d308cdc9
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-07-12 09:48:56 +0800

    B3836 [GESP202303 二级] 百鸡问题

---
+++ B3953 [GESP202403 一级] 找因数.cpp
@@ -0,0 +1,15 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
