e759b6ac1f2712a88a7362e0fe8a01637c650eb3
拾光 <picktime@Mac-mini.local>
2025-07-12 09:48:56 +0800

    B3836 [GESP202303 二级] 百鸡问题

---
+++ B3836 [GESP202303 二级] 百鸡问题.cpp
@@ -0,0 +1,19 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,z,n,m,cnt=0;
+    cin >> x >> y >> z >> n >> m;
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= m; j++) {
+            int k = m - i - j;
+            if (k >= 0 && k % z == 0 && i * x + j * y + k / z == n) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
