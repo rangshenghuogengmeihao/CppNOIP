a5e4b41a718e9801cf1f589f70a47573f564126a
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-07-25 13:07:18 +0800

    B4035 [GESP202409 一级] 美丽数字

---
+++ B3838 [GESP202306 一级] 时间规划.cpp
@@ -0,0 +1,11 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,sum=0;
+    cin >> a >> b >> c >> d;
+    sum = (c-a)*60+(d-b);
+    cout << sum << endl;
+}
