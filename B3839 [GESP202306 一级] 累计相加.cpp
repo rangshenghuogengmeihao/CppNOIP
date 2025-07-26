5da42970d33c43b65240ee02914602fe265313c2
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-07-26 14:39:01 +0800

    B3839 [GESP202306 一级] 累计相加

---
+++ B3839 [GESP202306 一级] 累计相加.cpp
@@ -0,0 +1,14 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        sum+=(1+i)*i/2;
+    }
+    cout << sum << endl;
+    return 0;
+}
