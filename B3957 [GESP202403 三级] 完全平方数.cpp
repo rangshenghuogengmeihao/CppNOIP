fecd799688d49390f984d752243829276319a636
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-08-01 17:09:36 +0800

    B4355 [GESP202506 一级] 值日

---
+++ B3957 [GESP202403 三级] 完全平方数.cpp
@@ -0,0 +1,25 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <cmath>
+#include <iostream>
+using namespace std;
+int a[1010];
+int main(){
+    int n,k,t,count=0;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    for (int i=1;i<=n-1;i++){
+        for (int j=i+1;j<=n;j++){
+            k = a[i]+a[j];
+            t = sqrt(k);
+            if (t*t==k){
+                count++;
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
---
+++ B4355 [GESP202506 一级] 值日.cpp
@@ -0,0 +1,16 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cin >> m >> n;
+    int i = 1;
+    for (int i=1;;i++){
+      if (i%m==0&&i%n==0){
+        cout << i << endl;
+        break;
+      }
+    }
+}
