06999d06e812f3d01baa2f7708a6712d76aabf7a
拾光 <picktime@Mac-mini.local>
2025-08-01 17:09:36 +0800

    B4355 [GESP202506 一级] 值日

---
+++ B4354 [GESP202506 一级] 假期阅读.cpp
@@ -0,0 +1,15 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n,k,t;
+    cin >> n >> k >> t;
+    if (k*t>n){
+        cout << n;
+    } else {
+        cout << k*t;
+    }
+    return 0;
+}
