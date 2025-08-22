8602857e7104eda5deaec8ee2279a52436d4f393
拾光 <picktime@Mac-mini.local>
2025-08-22 17:04:31 +0800

    B4063 [GESP202412 一级] 奇数和偶数

---
+++ B4062 [GESP202412 一级] 温度转换.cpp
@@ -0,0 +1,17 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    double k,c,f;
+    cin >> k;
+    c = k - 273.15;
+    f = c * 1.8 + 32;
+    if (f > 212){
+        cout << "Temperature is too high!";
+    }else{
+        printf("%.2f %.2f",c,f);
+    }
+    return 0;
+}
