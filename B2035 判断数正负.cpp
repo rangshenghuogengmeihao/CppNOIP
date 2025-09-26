333787796169c9e12a8cebdf5d29df4585961390
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2025-09-26 19:40:18 +0800

    B4356 [GESP202506 二级] 数三角形

---
+++ B2035 判断数正负.cpp
@@ -0,0 +1,16 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    if (x>0)
+        cout << "positive";
+    else if (x<0)
+        cout << "negative";
+    else
+        cout << "zero";
+    return 0;
+}
---
+++ B4357 [GESP202506 二级] 幂和数.cpp
@@ -0,0 +1,25 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int l,r,cnt=0;
+    cin >> l >> r;
+    for (int i = l; i <= r; i++) {
+        for (int x = 0;x <= 14;x++){
+            bool flag=false;
+            for (int y = 0;y <= 14;y++){
+                if (pow(2,x)+pow(2,y)==i){
+                    flag=true;
+                    cnt++;
+                    break;
+                }
+            }
+            if (flag)
+                break;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
