8867ebc9f9dbbb01b74782b843fd8d68780c81ef
拾光 <picktime@Mac-mini.local>
2025-12-30 17:17:34 +0800

    B4003 [GESP202406 三级] 移位

---
+++ B4003 [GESP202406 三级] 移位.cpp
@@ -0,0 +1,13 @@
+/*
+  @author:拾光 <rangshenghuogengmeihao@github.com>
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for (int i=0;i<26;i++){
+        char c = 'A'+(n+i)%26;
+        cout << c;
+    }
+}
