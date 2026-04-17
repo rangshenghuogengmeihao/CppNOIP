24b492c1130102a8d6343098e1b28b624a5f6b07
拾光 <90822094+rangshenghuogengmeihao@users.noreply.github.com>
2026-04-17 17:09:32 +0800

    B3866 [GESP202309 二级] 数字黑洞

--- B3866 [GESP202309 二级] 数字黑洞.cpp
+++ B3866 [GESP202309 二级] 数字黑洞.cpp
@@ -1,20 +1,41 @@
 /*
   @author:拾光 <rangshenghuogengmeihao@github.com>
 */
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 using namespace std;
 int a[3];
 int main() {
-    int n,res=0;
+    int n,cnt=0;
     cin >> n;
     while (n != 495) {
         a[0] = n % 10;
         a[1] = n / 10 % 10;
         a[2] = n / 100;
         sort(a,a+3);
-        n = a[2]*100 + a[1]*10 + a[0]-(a[0]*100+a[1]*10+a[2]);
-        res++;
+        n = (a[2]*100+a[1]*10+a[0]) - (a[0]*100+a[1]*10+a[2]);
+        cnt++;
     }
-    cout << res << endl;
+    cout << cnt << endl;
     return 0;
 }
+/*
+#include<iostream>
+using namespace std;
+int main() {
+    int n,cnt=0;
+    cin >> n;
+    while (n != 495) {
+        int a = n % 10;
+        int b = n / 10 % 10;
+        int c = n / 100;
+        if (a > b){swap(a,b);}
+        if (a > c){swap(a,c);}
+        if (b > c){swap(b,c);}
+        n = (c*100+b*10+a) - (a*100+b*10+c);
+        cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
+*/
